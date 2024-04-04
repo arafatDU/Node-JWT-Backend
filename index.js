08f801a5aea9f71b0c8a996e281e64d44f7d0326
ARAFAT <munshiarafathussain@gmail.com>
2024-04-04 16:54:33 +0600

    project structure setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const connectDB = require('./db/connect.js');
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const mongodb_connect = process.env.MONGO_URI;
 const app = express();
 
@@ -12,10 +13,19 @@ app.use(express.json());
 // routes
 
 
+
+
 app.get('/', (req, res) => {
     res.send('<h1>JWT Basic</h1>');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const start = async () => {
+    try {
+      await connectDB(mongodb_connect);
+      app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
+    } catch (error) {
+      console.error(error);
+    }
+}
+  
+start();
